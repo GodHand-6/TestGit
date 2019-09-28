ff489a75b9211932fc67532364b62b6e84f7d969
eclipse_user <eclipse_user@qlgydx.com>
2019-09-28 13:59:39 +0800

    test confilct right

--- src/main/java/com/qlgydx/git/Happy.java
+++ src/main/java/com/qlgydx/git/Happy.java
@@ -2,7 +2,7 @@ package com.qlgydx.git;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Test....");
+		System.out.println("Right....");
 		System.out.println("这是新改动");
 	}
 }

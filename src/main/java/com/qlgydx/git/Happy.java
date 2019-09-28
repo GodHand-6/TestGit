97686c24bf0357a0f8f3824efd66da2ba77b909c
GodHand-6 <1975597979@qq.com>
2019-09-28 14:19:40 +0800

    edit on branch hot_fix

--- src/main/java/com/qlgydx/git/Happy.java
+++ src/main/java/com/qlgydx/git/Happy.java
@@ -3,7 +3,6 @@ package com.qlgydx.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Left....");
-		System.out.println("Right....");
-		System.out.println("这是新改动");
+		System.out.println("hot fix......");
 	}
 }

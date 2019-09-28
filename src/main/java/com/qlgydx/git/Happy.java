653adb1e9dc17aea52209895984b69eeda9ac5c3
GodHand-6 <1975597979@qq.com>
2019-09-28 14:00:03 +0800

    test conflict left

--- src/main/java/com/qlgydx/git/Happy.java
+++ src/main/java/com/qlgydx/git/Happy.java
@@ -2,7 +2,7 @@ package com.qlgydx.git;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Test....");
+		System.out.println("Left....");
 		System.out.println("这是新改动");
 	}
 }

7199b4bdd3565276c701cbed5c8b821615aa3ffb
eclipse_user <eclipse_user@qlgydx.com>
2019-09-28 14:24:59 +0800

    edit for branch hot_fix

--- src/main/java/com/qlgydx/git/Happy.java
+++ src/main/java/com/qlgydx/git/Happy.java
@@ -3,6 +3,7 @@ package com.qlgydx.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Right....");
+		System.out.println("hot fix_xxx");
 		System.out.println("这是新改动");
 	}
 }
